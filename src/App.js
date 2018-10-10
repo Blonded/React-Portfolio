920da5d8a3de803734923cdfda6a613c096752f3
Blonded <lmartinson20@gmail.com>
2018-10-09 18:52:46 -0700

    added background img

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import bg_img from './clouds.jpg';
 
 class App extends Component {
   render() {
+    document.body.style.backgroundImage = `url("https://unsplash.com/photos/MF9Wy1NA55I")`;
     return (
       <div className="App">
         <header className="App-header">
+        <div className="Background-img">
+        <img alt="" className="" src={bg_img} width="1450px" />
+        </div>
           
           <p>
             Edit <code>src/App.js</code> and save to reload.
+
           </p>
           <a
             className="App-link"
