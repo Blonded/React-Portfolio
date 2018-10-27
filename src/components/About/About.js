27725525346ee832f4303a098b50a3c65a9ac87f
Blonded <lmartinson20@gmail.com>
2018-10-27 14:27:41 -0700

    styling the about component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,19 +14,20 @@ const About = () => {
         <Grid>
 				<Row>
 					<Col xs={12} sm={12} md={12}>
-                    <h1 className= "about">About</h1>
+                    
 					</Col>
 				</Row>
         
                 <Row>
 					<Col xs={6} sm={6} md={6}>
-                
-                </Col>
-                <Col xs={6} sm={6} md={6}>
-                <p className="about-details">
-                Hey there! My name is Lena and i'm a full-stack web developer currently residing in the Bay Area [San Francisco]. Coming from a creative background i've always been passionate about pushing myself artistically, enjoying the many ranges and paths it can take to have someones ideas come to life - or even my own!
-                </p>
-                </Col>
+                        <img src="/assets/imgs/lena-edit-0.png" alt="Logo"/>
+                    </Col>
+                    <Col xs={6} sm={6} md={6}>
+                        <h1 className= "about">About</h1>
+                        <p className="about-details">
+                        Hey there! My name is Lena and i'm a full-stack web developer currently residing in the Bay Area [San Francisco]. Coming from a creative background i've always been passionate about pushing myself artistically, enjoying the many ranges and paths it can take to have someones ideas come to life - or even my own!
+                        </p>
+                    </Col>
 
                 </Row>
 
