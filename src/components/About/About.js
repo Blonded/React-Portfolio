f609c3872f3a7c5bca963e454dccfe9e60959aaf
Blonded <lmartinson20@gmail.com>
2018-10-27 14:30:22 -0700

    styling

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,18 +12,14 @@ const About = () => {
             <Card className='about-card'>
             <div className="card-body">
         <Grid>
-				<Row>
-					<Col xs={12} sm={12} md={12}>
-                    
-					</Col>
-				</Row>
-        
                 <Row>
 					<Col xs={6} sm={6} md={6}>
                         <img src="/assets/imgs/lena-edit-0.png" alt="Logo"/>
                     </Col>
                     <Col xs={6} sm={6} md={6}>
                         <h1 className= "about">About</h1>
+                    </Col>
+                    <Col xs={6} sm={6} md={6}>
                         <p className="about-details">
                         Hey there! My name is Lena and i'm a full-stack web developer currently residing in the Bay Area [San Francisco]. Coming from a creative background i've always been passionate about pushing myself artistically, enjoying the many ranges and paths it can take to have someones ideas come to life - or even my own!
                         </p>
