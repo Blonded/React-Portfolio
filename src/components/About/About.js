a6bf77422fa793cad2789f365ebbe47ba31f0347
Blonded <lmartinson20@gmail.com>
2018-11-06 18:33:34 -0800

    adding icons in about for connecting

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,6 +5,8 @@ import { Grid, Row, Col} from 'react-bootstrap';
 
 
 class About extends Component { 
+    
+
     render(){
         return(
             <Grid>
@@ -17,9 +19,20 @@ class About extends Component {
                         <p>
                         Hey there! My name is Lena and i'm a full-stack web developer currently residing in the Bay Area [San Francisco]. Coming from a creative background i've always been passionate about pushing myself artistically, enjoying the many ranges and paths it can take to have someones ideas come to life - or even my own!
                         </p>
+                        <div className="connect">
+                        <a href="https://github.com/Blonded" target="_blank"  title="github">
+
+                        <a href="www.linkedin.com/in/lena-m-73b471161" target="_blank" title="linkedin"></a>
+
+                        <a href="" target="_blank" title="email"></a>
+
+                        <a href="" target="_blank" title="resume"></a>
+
+
+                        </div>
                     </Col>
                 </Row>
-        </Grid>
+            </Grid>
                 )
             }
 }
