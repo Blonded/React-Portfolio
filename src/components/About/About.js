6b7872426742a66c4462f413604f11296c67e2c1
Blonded <lmartinson20@gmail.com>
2018-11-06 18:07:10 -0800

    mild alterations and cleanup

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,39 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './style.css';
 import { Grid, Row, Col} from 'react-bootstrap';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 
 
 
-const About = () => {
-    return(
-        <div className="about-description">
-            <Card className='about-card'>
-            <div className="card-body">
-        <Grid>
-                <Row>
-					<Col xs={6} sm={6} md={6}>
+class About extends Component { 
+    render(){
+        return(
+            <Grid>
+                <Row id="about">
+					<Col xs={12} sm={12} md={6} align="left" className="img-wrap">
                         <img src="/assets/imgs/lena-edit-0.png" alt="aboutphoto"/>
                     </Col>
-                    <Col xs={6} sm={6} md={6}>
-                        <h1 className= "about">About</h1>
-                    </Col>
-                    <Col xs={6} sm={6} md={6}>
-                        <p className="about-details">
+                    <Col xs={12} sm={12} md={6} className= "bio">
+                       <h1>About</h1>
+                        <p>
                         Hey there! My name is Lena and i'm a full-stack web developer currently residing in the Bay Area [San Francisco]. Coming from a creative background i've always been passionate about pushing myself artistically, enjoying the many ranges and paths it can take to have someones ideas come to life - or even my own!
                         </p>
                     </Col>
-
                 </Row>
-
-
         </Grid>
-        </div>
-        </Card>
-        </div> 
-       
-    )
+                )
+            }
 }
 
 export default About; 
