8502443978186b549bb29511a3d78657245e12b6
Blonded <lmartinson20@gmail.com>
2019-02-09 17:09:31 -0800

    fixed error

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,13 +14,14 @@ class About extends Component {
 					<Col xs={12} sm={12} md={6} align="left" className="img-wrap">
                         <img src="/assets/imgs/lena-edit-0.png" alt="aboutphoto"/>
                     </Col>
+
                     <Col xs={12} sm={12} md={6} className= "bio">
                        <h1>About</h1>
                         <p>
                         Hey there! My name is Lena and i'm a full-stack web developer currently residing in the Bay Area [San Francisco]. Coming from a creative background i've always been passionate about pushing myself artistically, enjoying the many ranges and paths it can take to have someones ideas come to life - or even my own!
                         </p>
                         <div className="connect">
-                        <a href="https://github.com/Blonded" target="_blank"  title="github">
+                        <a href="https://github.com/Blonded" target="_blank"  title="github"></a>
 
                         <a href="www.linkedin.com/in/lena-m-73b471161" target="_blank" title="linkedin"></a>
 
