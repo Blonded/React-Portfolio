a376e80dac07b6f16160cb38bb0ec91e47e063d5
Blonded <lmartinson20@gmail.com>
2019-06-02 20:02:43 -0700

    Updates

---
+++ precache-manifest.f6ac94128eed917411d324b4333f0db2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "804d233a10add83efa8d",
+    "url": "/React-Portfolio/static/js/runtime~main.804d233a.js"
+  },
+  {
+    "revision": "936e8180dc57041a02df",
+    "url": "/React-Portfolio/static/js/main.936e8180.chunk.js"
+  },
+  {
+    "revision": "5faf7cb3e64db1d756df",
+    "url": "/React-Portfolio/static/js/1.5faf7cb3.chunk.js"
+  },
+  {
+    "revision": "936e8180dc57041a02df",
+    "url": "/React-Portfolio/static/css/main.2fb7fe23.chunk.css"
+  },
+  {
+    "revision": "c2144f2b0a43c841f5da146515e02abf",
+    "url": "/React-Portfolio/index.html"
+  }
+];
